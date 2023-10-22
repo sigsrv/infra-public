db5ded02494ee0b973470cc3e584d062e20a5883
EcmaXp <ecmaxp@ecmaxp.kr>
2023-10-23 01:35:15 +0900

    ~ 20231023

--- tools/lxc.py
+++ tools/lxc.py
@@ -8,15 +8,20 @@ def main():
     argv = sys.argv[:]
     argv[0] = "lxc"
 
-    if "--project" not in argv:
+    if "--" in argv:
+        pos = argv.index("--")
+    else:
+        pos = len(argv)
+
+    if "--project" not in argv[:pos]:
         if lxc_project := os.getenv("LXC_PROJECT"):
             argv[1:1] += ["--project", lxc_project]
 
-    if "list" in argv:
+    if "list" in argv[:pos]:
         argv[1:1] += ["--format", "compact"]
 
     os.execl(shutil.which("lxc"), *argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
