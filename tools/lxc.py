3cb854d8676baabf2712ac3256bbd0e27802aa8e
EcmaXp <ecmaxp@ecmaxp.kr>
2023-11-12 22:11:18 +0900

    fix lxc shell command

--- tools/lxc.py
+++ tools/lxc.py
@@ -15,7 +15,14 @@ def main():
 
     if "--project" not in argv[:pos]:
         if lxc_project := os.getenv("LXC_PROJECT"):
-            argv[1:1] += ["--project", lxc_project]
+            for command in ["shell"]:
+                if command in argv[:pos]:
+                    idx = argv.index(command) + 1
+                    break
+            else:
+                idx = 1
+
+            argv[idx:idx] += ["--project", lxc_project]
             pos += 2
 
     if "list" in argv[:pos] and "device" not in argv[:pos]:
