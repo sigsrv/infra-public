39bc0539e82bca3d900ce7df6c72b93b0c832c33
EcmaXp <ecmaxp@ecmaxp.kr>
2023-11-25 22:30:15 +0900

    fix tailscale client id

--- root/ryzen/microk8s/addons/tailscale/create-tailsacle-secrets.sh
+++ root/ryzen/microk8s/addons/tailscale/create-tailsacle-secrets.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 kubectl create secret generic operator-oauth \
   -n tailscale \
-  --from-literal=client_id=ku9Q2q5CNTRL \
+  --from-literal=client_id=kSEoeK5CNTRL \
   --from-literal=client_secret=`pbpaste`
