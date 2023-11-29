aa4004fbfe9c66d9d0418b993d68bcbae63bb410
EcmaXp <ecmaxp@ecmaxp.kr>
2023-11-30 00:25:19 +0900

    change tailsacle oauth client

--- root/ryzen/k3s/addons/tailscale/create-tailsacle-secrets.sh
+++ root/ryzen/k3s/addons/tailscale/create-tailsacle-secrets.sh
@@ -2,5 +2,5 @@
 # https://login.tailscale.com/admin/settings/oauth
 kubectl create secret generic operator-oauth \
   -n tailscale \
-  --from-literal=client_id=ktXnVK6CNTRL \
+  --from-literal=client_id=kxKajA7CNTRL \
   --from-literal=client_secret=`pbpaste`
