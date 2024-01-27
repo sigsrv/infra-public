690ccae4fea4badbfc8979f4cd391fc8edb74adc
EcmaXp <ecmaxp@ecmaxp.kr>
2024-01-28 03:36:45 +0900

    add trust-manager

--- zen/sdlc/sigsrv-sdlc/root/k3s/addons/cert-manager/install-cert-manager.fish
+++ zen/sdlc/sigsrv-sdlc/root/k3s/addons/cert-manager/install-cert-manager.fish
@@ -2,3 +2,4 @@
 kubectl --context sigsrv-sdlc create ns cert-manager
 kubectl --context sigsrv-sdlc apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.crds.yaml
 kubectl --context sigsrv-sdlc apply -f cert-manager.yaml
+kubectl --context sigsrv-sdlc apply -f trust-manager.yaml
