51dbc55ca2f8bc063fa38a9a31bb009e041c4c28
EcmaXp <ecmaxp@ecmaxp.kr>
2024-01-27 23:10:33 +0900

    add cert-manager in sigsrv-sdlc

---
+++ zen/sdlc/sigsrv-sdlc/root/k3s/addons/cert-manager/install-cert-manager.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+kubectl --context sigsrv-sdlc create ns cert-manager
+kubectl --context sigsrv-sdlc apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.crds.yaml
+kubectl --context sigsrv-sdlc apply -f cert-manager.yaml
