7e5d8d93f643989d19958f68d73267b6abfc7d31
EcmaXp <ecmaxp@ecmaxp.kr>
2024-02-03 14:16:25 +0900

    terraform fmt

--- aws/root/controltower/controls/config.tf
+++ aws/root/controltower/controls/config.tf
@@ -5,7 +5,7 @@ variable "aws_profile" {
 
 variable "aws_account_id" {
   type    = string
-  default = "607292096646"  # sigsrv
+  default = "607292096646" # sigsrv
 }
 
 variable "aws_region" {
@@ -14,7 +14,7 @@ variable "aws_region" {
 }
 
 variable "enabled_safe_controls" {
-  type    = map(list(string))
+  type = map(list(string))
   default = {
     "Sandbox" : [
       "AWS-GR_DETECT_CLOUDTRAIL_ENABLED_ON_MEMBER_ACCOUNTS",
@@ -53,68 +53,68 @@ variable "enabled_safe_controls" {
 
 //noinspection SpellCheckingInspection
 variable "enabled_unsafe_controls" {
-  type    = map(map(string))
+  type = map(map(string))
   //noinspection TFIncorrectVariableType
   default = {
-    #    "Sandbox" : {
-    #      "CT.CLOUDFORMATION.PR.1" : "AZAYRJYXCXZR",
-    #      "CT.CLOUDFRONT.PR.1" : "VBJDOHQSIIAX",
-    #      "CT.CODEBUILD.PR.1" : "XWOSMSCORYXY",
-    #      "CT.CODEBUILD.PR.2" : "EOOPASTMJSET",
-    #      "CT.DMS.PR.1" : "PMXDPOCYQXVY",
-    #      "CT.EC2.PR.4" : "YTLKASXYASWH",
-    #      "CT.LAMBDA.PR.2" : "LNBOCFPQISPJ",
-    #      "CT.OPENSEARCH.PR.10" : "XRNYJUFPDRET",
-    #      "CT.OPENSEARCH.PR.2" : "UCLVYXAVOAHM",
-    #      "CT.RDS.PR.23" : "NNNWMZYHHRLE",
-    #      "CT.REDSHIFT.PR.1" : "XOLIHOFJYQAC",
-    #      "SH.CodeBuild.1" : "TIHGQHSIUQES",
-    #      "SH.CodeBuild.2" : "IHLKDRJWXUHX",
-    #      "SH.DMS.1" : "KKTKHLMBCETI",
-    #      "SH.EC2.1" : "OSTTYXVZPAKB",
-    #      "SH.EC2.19" : "VHSRRHUYSMZF",
-    #      "SH.ES.2" : "TPVVQKJODFVL",
-    #      "SH.IAM.4" : "KNDGXYOAJBAD",
-    #      "SH.IAM.6" : "BOIRCXQEYNLQ",
-    #      "SH.KMS.3" : "IRPUDLZBQQWO",
-    #      "SH.Lambda.1" : "FQMQXEDYMYNK",
-    #      "SH.Opensearch.2" : "NPHMMQWTNQJA",
-    #      "SH.RDS.1" : "JZXYYUXYDVES",
-    #      "SH.RDS.2" : "LVCJBGBIVJAD",
-    #      "SH.Redshift.1" : "KUKRAWPCWPWD",
-    #      "SH.S3.2" : "JVTGANIDGRYD",
-    #      "SH.S3.3" : "UQFGRBNKRVKM",
-    #      "SH.SSM.4" : "UDAQAMUOGDDV",
-    #    },
-#    "Workloads" : {
-#      "CT.CLOUDFORMATION.PR.1" : "AZAYRJYXCXZR",
-#      "CT.CLOUDFRONT.PR.1" : "VBJDOHQSIIAX",
-#      #      "CT.CODEBUILD.PR.1" : "XWOSMSCORYXY",
-#      #      "CT.CODEBUILD.PR.2" : "EOOPASTMJSET",
-#      #      "CT.DMS.PR.1" : "PMXDPOCYQXVY",
-#      "CT.EC2.PR.4" : "YTLKASXYASWH",
-#      "CT.LAMBDA.PR.2" : "LNBOCFPQISPJ",
-#      #      "CT.OPENSEARCH.PR.10" : "XRNYJUFPDRET",
-#      #      "CT.OPENSEARCH.PR.2" : "UCLVYXAVOAHM",
-#      "CT.RDS.PR.23" : "NNNWMZYHHRLE",
-#      #      "CT.REDSHIFT.PR.1" : "XOLIHOFJYQAC",
-#      #      "SH.CodeBuild.1" : "TIHGQHSIUQES",
-#      #      "SH.CodeBuild.2" : "IHLKDRJWXUHX",
-#      #      "SH.DMS.1" : "KKTKHLMBCETI",
-#      "SH.EC2.1" : "OSTTYXVZPAKB",
-#      "SH.EC2.19" : "VHSRRHUYSMZF",
-#      #      "SH.ES.2" : "TPVVQKJODFVL",
-#      "SH.IAM.4" : "KNDGXYOAJBAD",
-#      "SH.IAM.6" : "BOIRCXQEYNLQ",
-#      "SH.KMS.3" : "IRPUDLZBQQWO",
-#      "SH.Lambda.1" : "FQMQXEDYMYNK",
-#      #      "SH.Opensearch.2" : "NPHMMQWTNQJA",
-#      "SH.RDS.1" : "JZXYYUXYDVES",
-#      "SH.RDS.2" : "LVCJBGBIVJAD",
-#      #      "SH.Redshift.1" : "KUKRAWPCWPWD",
-#      "SH.S3.2" : "JVTGANIDGRYD",
-#      "SH.S3.3" : "UQFGRBNKRVKM",
-#      "SH.SSM.4" : "UDAQAMUOGDDV"
-#    },
+    "Sandbox" : {
+      # "CT.CLOUDFORMATION.PR.1" : "AZAYRJYXCXZR",
+      # "CT.CLOUDFRONT.PR.1" : "VBJDOHQSIIAX",
+      # "CT.CODEBUILD.PR.1" : "XWOSMSCORYXY",
+      # "CT.CODEBUILD.PR.2" : "EOOPASTMJSET",
+      # "CT.DMS.PR.1" : "PMXDPOCYQXVY",
+      # "CT.EC2.PR.4" : "YTLKASXYASWH",
+      # "CT.LAMBDA.PR.2" : "LNBOCFPQISPJ",
+      # "CT.OPENSEARCH.PR.10" : "XRNYJUFPDRET",
+      # "CT.OPENSEARCH.PR.2" : "UCLVYXAVOAHM",
+      # "CT.RDS.PR.23" : "NNNWMZYHHRLE",
+      # "CT.REDSHIFT.PR.1" : "XOLIHOFJYQAC",
+      # "SH.CodeBuild.1" : "TIHGQHSIUQES",
+      # "SH.CodeBuild.2" : "IHLKDRJWXUHX",
+      # "SH.DMS.1" : "KKTKHLMBCETI",
+      # "SH.EC2.1" : "OSTTYXVZPAKB",
+      # "SH.EC2.19" : "VHSRRHUYSMZF",
+      # "SH.ES.2" : "TPVVQKJODFVL",
+      # "SH.IAM.4" : "KNDGXYOAJBAD",
+      # "SH.IAM.6" : "BOIRCXQEYNLQ",
+      # "SH.KMS.3" : "IRPUDLZBQQWO",
+      # "SH.Lambda.1" : "FQMQXEDYMYNK",
+      # "SH.Opensearch.2" : "NPHMMQWTNQJA",
+      # "SH.RDS.1" : "JZXYYUXYDVES",
+      # "SH.RDS.2" : "LVCJBGBIVJAD",
+      # "SH.Redshift.1" : "KUKRAWPCWPWD",
+      # "SH.S3.2" : "JVTGANIDGRYD",
+      # "SH.S3.3" : "UQFGRBNKRVKM",
+      # "SH.SSM.4" : "UDAQAMUOGDDV",
+    },
+    "Workloads" : {
+      # "CT.CLOUDFORMATION.PR.1" : "AZAYRJYXCXZR",
+      # "CT.CLOUDFRONT.PR.1" : "VBJDOHQSIIAX",
+      # "CT.CODEBUILD.PR.1" : "XWOSMSCORYXY",
+      # "CT.CODEBUILD.PR.2" : "EOOPASTMJSET",
+      # "CT.DMS.PR.1" : "PMXDPOCYQXVY",
+      # "CT.EC2.PR.4" : "YTLKASXYASWH",
+      # "CT.LAMBDA.PR.2" : "LNBOCFPQISPJ",
+      # "CT.OPENSEARCH.PR.10" : "XRNYJUFPDRET",
+      # "CT.OPENSEARCH.PR.2" : "UCLVYXAVOAHM",
+      # "CT.RDS.PR.23" : "NNNWMZYHHRLE",
+      # "CT.REDSHIFT.PR.1" : "XOLIHOFJYQAC",
+      # "SH.CodeBuild.1" : "TIHGQHSIUQES",
+      # "SH.CodeBuild.2" : "IHLKDRJWXUHX",
+      # "SH.DMS.1" : "KKTKHLMBCETI",
+      # "SH.EC2.1" : "OSTTYXVZPAKB",
+      # "SH.EC2.19" : "VHSRRHUYSMZF",
+      # "SH.ES.2" : "TPVVQKJODFVL",
+      # "SH.IAM.4" : "KNDGXYOAJBAD",
+      # "SH.IAM.6" : "BOIRCXQEYNLQ",
+      # "SH.KMS.3" : "IRPUDLZBQQWO",
+      # "SH.Lambda.1" : "FQMQXEDYMYNK",
+      # "SH.Opensearch.2" : "NPHMMQWTNQJA",
+      # "SH.RDS.1" : "JZXYYUXYDVES",
+      # "SH.RDS.2" : "LVCJBGBIVJAD",
+      # "SH.Redshift.1" : "KUKRAWPCWPWD",
+      # "SH.S3.2" : "JVTGANIDGRYD",
+      # "SH.S3.3" : "UQFGRBNKRVKM",
+      # "SH.SSM.4" : "UDAQAMUOGDDV"
+    },
   }
 }
