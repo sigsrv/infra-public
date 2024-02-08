dbe39ed6c9b415f87d805a3ce8c63422a3b89500
EcmaXp <ecmaxp@ecmaxp.kr>
2024-02-08 23:20:15 +0900

    remove traefik before install istio

--- zen/sdlc/sigsrv-sdlc/root/k3s/addons/istio/install-istio.fish
+++ zen/sdlc/sigsrv-sdlc/root/k3s/addons/istio/install-istio.fish
@@ -1,4 +1,6 @@
 #!/usr/bin/env fish
+kubectl delete helmcharts.helm.cattle.io -n kube-system traefik
+kubectl delete helmcharts.helm.cattle.io -n kube-system traefik-crd
 kubectl --context sigsrv-sdlc create namespace istio-system
 kubectl --context sigsrv-sdlc apply -f istio-system.yaml
 kubectl --context sigsrv-sdlc create namespace istio-ingress
