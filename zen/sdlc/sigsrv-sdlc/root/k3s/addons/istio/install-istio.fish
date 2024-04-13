202c304a3afcdbce1825b5e40fde9a5f601cc687
ecmaxp <ecmaxp@ecmaxp.kr>
2024-04-13 20:27:35 +0900

    merge istio-ingress into istio-system

--- zen/sdlc/sigsrv-sdlc/root/k3s/addons/istio/install-istio.fish
+++ zen/sdlc/sigsrv-sdlc/root/k3s/addons/istio/install-istio.fish
@@ -1,7 +1,3 @@
 #!/usr/bin/env fish
-kubectl delete helmcharts.helm.cattle.io -n kube-system traefik
-kubectl delete helmcharts.helm.cattle.io -n kube-system traefik-crd
 kubectl --context sigsrv-sdlc create namespace istio-system
 kubectl --context sigsrv-sdlc apply -f istio-system.yaml
-kubectl --context sigsrv-sdlc create namespace istio-ingress
-kubectl --context sigsrv-sdlc apply -f istio-ingress.yaml
