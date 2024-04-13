2ab4007877be1153eaf8123173f2a0ab66605d7a
ecmaxp <ecmaxp@ecmaxp.kr>
2024-04-13 19:47:47 +0900

    setup argocd repo

--- zen/sdlc/sigsrv-sdlc/root/k3s/addons/argocd/install-argocd.fish
+++ zen/sdlc/sigsrv-sdlc/root/k3s/addons/argocd/install-argocd.fish
@@ -2,3 +2,16 @@
 kubectl --context sigsrv-sdlc create ns argocd
 kubectl --context sigsrv-sdlc apply -k "https://github.com/argoproj/argo-cd/manifests/crds?ref=stable"
 kubectl --context sigsrv-sdlc apply -f argocd.yaml
+
+argocd login argocd-sdlc.deer-neon.ts.net \
+    --username admin \
+    --password (kubectl view-secret --context sigsrv-sdlc -n argocd argocd-initial-admin-secret password -q)
+
+set OP_ARGOCD_SSH_SECRET "op://sigsrv-sdlc/sigsrv-sdlc-argocd-ssh/private_key"
+op read $OP_ARGOCD_SSH_SECRET  > /tmp/argocd-ssh.key
+argocd repo add \
+    --name sigsrv-infra \
+    --project default \
+    --ssh-private-key-path=/tmp/argocd-ssh.key \
+    git@github.com:sigsrv/infra.git
+rm -f /tmp/argocd-ssh.key
