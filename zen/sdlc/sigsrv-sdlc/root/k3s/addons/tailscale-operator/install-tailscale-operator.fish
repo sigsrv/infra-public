7533935349cba526f7a891db2168bd5cade381f5
ecmaxp <ecmaxp@ecmaxp.kr>
2024-04-13 23:28:36 +0900

    setup tailscale-operator with argocd

--- zen/sdlc/sigsrv-sdlc/root/k3s/addons/tailscale-operator/install-tailscale-operator.fish
+++ zen/sdlc/sigsrv-sdlc/root/k3s/addons/tailscale-operator/install-tailscale-operator.fish
@@ -1,15 +1,6 @@
 #!/usr/bin/env fish
-helm upgrade \
-  --install \
-  --kube-context sigsrv-sdlc \
-  tailscale-operator \
-  tailscale/tailscale-operator \
-  --namespace=tailscale \
-  --create-namespace \
-  --set-string oauth.clientId=(op read "op://sigsrv-sdlc/sigsrv-sdlc-tailscale-operator/username") \
-  --set-string oauth.clientSecret=(op read "op://sigsrv-sdlc/sigsrv-sdlc-tailscale-operator/password") \
-  --set-string 'operatorConfig.hostname=sigsrv-sdlc-tailscale-operator' \
-  --set 'operatorConfig.defaultTags={tag:sigsrv-sdlc-tailscale-operator}' \
-  --set-string 'proxyConfig.defaultTags=tag:sigsrv-sdlc-tailscale-service' \
-  --set-string 'proxyConfig.apiServerProxyConfig.mode=true' \
-  --wait
+kubectl create secret generic \
+    --context sigsrv-sdlc \
+    -n tailscale operator-oauth \
+    --from-literal=client_id=(op read "op://sigsrv-sdlc/sigsrv-sdlc-tailscale-operator/username") \
+    --from-literal=client_secret=(op read "op://sigsrv-sdlc/sigsrv-sdlc-tailscale-operator/password")
