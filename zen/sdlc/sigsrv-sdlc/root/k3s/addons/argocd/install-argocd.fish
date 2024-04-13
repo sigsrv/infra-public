395ccfdb196b86b0f478c33efbf6adb21b4e4047
ecmaxp <ecmaxp@ecmaxp.kr>
2024-04-13 21:32:10 +0900

    setup addons by argocd cluster app

--- zen/sdlc/sigsrv-sdlc/root/k3s/addons/argocd/install-argocd.fish
+++ zen/sdlc/sigsrv-sdlc/root/k3s/addons/argocd/install-argocd.fish
@@ -7,6 +7,10 @@ argocd login argocd-sdlc.deer-neon.ts.net \
     --username admin \
     --password (kubectl view-secret --context sigsrv-sdlc -n argocd argocd-initial-admin-secret password -q)
 
+curl "https://keybase.io/ecmaxp/pgp_keys.asc" > pgp_keys.asc
+argocd gpg add --from pgp_keys.asc
+rm -f pgp_keys.asc
+
 set OP_ARGOCD_SSH_SECRET "op://sigsrv-sdlc/sigsrv-sdlc-argocd-ssh/private_key"
 op read $OP_ARGOCD_SSH_SECRET  > /tmp/argocd-ssh.key
 argocd repo add \
