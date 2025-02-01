8be85c46368b29a0228be3900b364dbcf60b80b0
ecmaxp <ecmaxp@ecmaxp.kr>
2025-02-02 03:02:15 +0900

    feat(tfstate): migrate local tfstate to s3 (2)

--- aws/root/apps/controltower_controls/terraform.tf
+++ aws/root/apps/controltower_controls/terraform.tf
@@ -22,37 +22,14 @@ terraform {
       keys = key_provider.aws_kms.kms_key
     }
 
-    # "op://sigsrv-prod/sigsrv-infra tfstate encryption/password"
-    key_provider "pbkdf2" "tfstate_encryption_passphrase" {
-      passphrase = var.tfstate_encryption_passphrase
-    }
-
-    method "aes_gcm" "tfstate_encryption_passphrase" {
-      keys = key_provider.pbkdf2.tfstate_encryption_passphrase
-    }
-
     state {
       enforced = true
       method   = method.aes_gcm.kms_key
-
-      fallback {
-        method = method.aes_gcm.tfstate_encryption_passphrase
-      }
     }
 
     plan {
       enforced = true
       method   = method.aes_gcm.kms_key
-
-      fallback {
-        method = method.aes_gcm.tfstate_encryption_passphrase
-      }
     }
   }
-}
-
-variable "tfstate_encryption_passphrase" {
-  type      = string
-  nullable  = false
-  sensitive = true
 }
