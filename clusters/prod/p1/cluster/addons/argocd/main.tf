b5e19a1daf2af381e5389588f75764c17a335559
ecmaxp <ecmaxp@ecmaxp.kr>
2025-03-02 16:14:30 +0900

    fix(talos/addons): add terraform_data resources for kubectl documents

--- clusters/prod/p1/cluster/addons/argocd/main.tf
+++ clusters/prod/p1/cluster/addons/argocd/main.tf
@@ -63,8 +63,12 @@ data "kubectl_file_documents" "this" {
   })
 }
 
+resource "terraform_data" "this" {
+  input = data.kubectl_file_documents.this.manifests
+}
+
 resource "kubectl_manifest" "this" {
-  for_each  = data.kubectl_file_documents.this.manifests
+  for_each  = terraform_data.this.output
   yaml_body = each.value
 
   depends_on = [
--- clusters/prod/p1/cluster/addons/local-path-provisioner/main.tf
+++ clusters/prod/p1/cluster/addons/local-path-provisioner/main.tf
@@ -6,7 +6,11 @@ data "kubectl_file_documents" "this" {
   content = join("\n---\n", data.kubectl_kustomize_documents.this.documents)
 }
 
+resource "terraform_data" "this" {
+  input = data.kubectl_file_documents.this.manifests
+}
+
 resource "kubectl_manifest" "this" {
-  for_each  = data.kubectl_file_documents.this.manifests
+  for_each  = terraform_data.this.output
   yaml_body = each.value
 }
