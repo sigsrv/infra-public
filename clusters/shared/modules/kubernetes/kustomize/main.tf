a3bc525688b61fc9fea0dada462cef573c601e5a
ecmaxp <ecmaxp@ecmaxp.kr>
2025-03-05 00:42:17 +0900

    fix(manifests,kustomize)!: create namespace resources first

--- clusters/shared/modules/kubernetes/kustomize/main.tf
+++ clusters/shared/modules/kubernetes/kustomize/main.tf
@@ -4,7 +4,8 @@ data "kubectl_kustomize_documents" "this" {
 
 locals {
   content = join("\n---\n", data.kubectl_kustomize_documents.this.documents)
-  manifests = {
+
+  all_manifests = {
     for manifest in provider::kubernetes::manifest_decode_multi(local.content) :
     join("/", [
       manifest["apiVersion"],
@@ -13,9 +14,28 @@ locals {
       manifest["metadata"]["name"]
     ]) => manifest
   }
+
+  namespace_manifests = {
+    for key, manifest in local.all_manifests : key => manifest
+    if manifest["kind"] == "Namespace"
+  }
+
+  manifests = {
+    for key, manifest in local.all_manifests : key => manifest
+    if manifest["kind"] != "Namespace"
+  }
+}
+
+resource "kubernetes_manifest" "namespace" {
+  for_each = toset(keys(local.namespace_manifests))
+  manifest = local.namespace_manifests[each.key]
 }
 
 resource "kubernetes_manifest" "this" {
   for_each = toset(keys(local.manifests))
   manifest = local.manifests[each.key]
+
+  depends_on = [
+    kubernetes_manifest.namespace,
+  ]
 }
--- clusters/shared/modules/kubernetes/manifests/main.tf
+++ clusters/shared/modules/kubernetes/manifests/main.tf
@@ -1,5 +1,5 @@
 locals {
-  manifests = {
+  all_manifests = {
     for manifest in provider::kubernetes::manifest_decode_multi(var.content) :
     join("/", [
       manifest["apiVersion"],
@@ -8,9 +8,28 @@ locals {
       manifest["metadata"]["name"]
     ]) => manifest
   }
+
+  namespace_manifests = {
+    for key, manifest in local.all_manifests : key => manifest
+    if manifest["kind"] == "Namespace"
+  }
+
+  manifests = {
+    for key, manifest in local.all_manifests : key => manifest
+    if manifest["kind"] != "Namespace"
+  }
+}
+
+resource "kubernetes_manifest" "namespace" {
+  for_each = toset(keys(local.namespace_manifests))
+  manifest = local.namespace_manifests[each.key]
 }
 
 resource "kubernetes_manifest" "this" {
   for_each = toset(keys(local.manifests))
   manifest = local.manifests[each.key]
+
+  depends_on = [
+    kubernetes_manifest.namespace,
+  ]
 }
